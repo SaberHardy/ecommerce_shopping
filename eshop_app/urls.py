2dcbaecd74b789db9541b05817869c5d9845f5f1
SaberHardy <samirsamado5@gmail.com>
2022-02-23 22:41:06 +0300

    add some views and extends

--- eshop_app/urls.py
+++ eshop_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shop/', views.shop, name='index'),
+    path('404/', views.err404, name='err404'),
 ]
--- eshop_app/views.py
+++ eshop_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'eshop_app/index.html', {})
+
+
+def shop(request):
+    return render(request, 'eshop_app/shop.html', {})
+
+
+def err404(request):
+    return render(request, 'eshop_app/404.html', {})
